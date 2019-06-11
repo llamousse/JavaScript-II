c9971334c416b19852f9860cae04089b500f4a96
Vicky <vyue013@gmail.com>
2019-06-11 16:18:11 -0700

    mvp completed

--- assignments/closure.js
+++ assignments/closure.js
@@ -13,14 +13,23 @@ console.log(drink());
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function() {
+    return count++;
+  }
 };
+
+const newCounter = counter();
+console.log(newCounter()); // 1
+// console.log(newCounter()); // 2
+// console.log(newCounter()); // 3
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
+// const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-};
