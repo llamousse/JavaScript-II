c6263ce35627ed30e53a9ad62d5bd29680d52809
Vicky <vyue013@gmail.com>
2019-06-11 13:46:24 -0700

    completed closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,11 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+const coffee = 'vanilla latte';
+function drink() {
+  return coffee;
+}
+drink();
+console.log(drink());
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
